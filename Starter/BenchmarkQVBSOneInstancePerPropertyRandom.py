bf8e5ed7aabbf2a043c493e73e1f5cc3c1e7280a
Vescator <Vescator>
2022-07-27 16:41:37 +0200

    Add random parameter per instance

---
+++ Starter/BenchmarkQVBSOneInstancePerPropertyRandom.py
@@ -0,0 +1,5 @@
+from Specific.Benchmarks.qvbs_benchmark_one_instance_per_property_random import \
+    QvbsBenchmarkOneInstancePerPropertyRandom
+
+benchmark = QvbsBenchmarkOneInstancePerPropertyRandom()
+benchmark.run()
